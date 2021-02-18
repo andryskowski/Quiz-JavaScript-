da0f45f2381637c93f5d4bcb8e457879e376578e
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-02-18 22:26:28 +0100

    added test to adding answer

--- Quiz_Application/modules_clear_js/Answers.test.js
+++ Quiz_Application/modules_clear_js/Answers.test.js
@@ -6,4 +6,12 @@ describe('totalAnswers', () => {
         const ANSWERS_LIST = subject.answersList;
         expect(ANSWERS_LIST.length).toBe(0)
     })
+    it('add answer', () =>{
+        const subject = new Answers();
+
+        subject.addAnswerToList('A');
+        const ANSWERS_LIST = subject.answersList;
+        const ACTUAL_COUNT = ANSWERS_LIST.length;
+        expect(ACTUAL_COUNT).toBeGreaterThan(0)
+    })
 })
